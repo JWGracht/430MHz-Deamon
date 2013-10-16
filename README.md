28cb1131804bcb21c844bf74d7ad0fe83cb2e554
JWGracht <jw.grachten@gmail.com>
2013-10-17 00:34:57 +0200

    first update

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Beagle Bone Black 430MHz GPIO deamon
 Requirements
 + adafruit-beaglebone-io-python
 + Any 430MHz signal transceiver (transmitter / receiver)
++ sudo apt-get install bc
 
 Supported protocols
 + Elro switches
---
+++ protocols/elro-send.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+system_code="10111"
+
+unit_code="01000" #A, B, C, D, E switch
+
+switch=1
+
+elroframe="$system_code$unit_code"
+
+elroframe=$( echo $elroframe | sed 's|1|136 |g' )
+elroframe=$( echo $elroframe | sed 's|0|142 |g' )
+
+if [ $switch -gt 0 ]; then
+  elroframe="${elroframe}136 142 "
+else
+  elroframe="${elroframe}142 136 "
+fi
+
+elroframe="${elroframe}128 0 0 0"
+
+echo $elroframe
+
+export IFS=" "
+
+buffer=""
+
+for framepart in $elroframe; do
+  printf "%0*d\n" 8 $(echo "obase=2;$framepart" | bc)
+  framepart=$(printf "%0*d\n" 8 $(echo "obase=2;$framepart" | bc))
+  buffer="$buffer$framepart"
+done
+
+elroframe=$buffer
+
+echo $elroframe
+
+#denk aan FIFO
