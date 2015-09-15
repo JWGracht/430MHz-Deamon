1695050169d5a3782b349597c1c7bbe405cce896
JWGracht <jw.grachten@gmail.com>
2015-09-15 21:43:31 +0200

    Update 430MHz-Deamon.py

--- 430MHz-Deamon.py
+++ 430MHz-Deamon.py
@@ -7,9 +7,9 @@ import serial
 
 class Tranceiver:
   repeat = 8 #later in config file
-  pulselength = 0.000260 # = 260 ms (verified with logic analyser) later in config file
+  pulselength = 0.000130 # = 130 ms (verified with logic analyser) later in config file
   transmitFIFO = '/tmp/transmitterfifo'
-  pin_out ="P8_07" #later in config file
+  pin_out ="P8_12" #later in config file
 
   def __init__(self):
     if os.path.exists(self.transmitFIFO) :
